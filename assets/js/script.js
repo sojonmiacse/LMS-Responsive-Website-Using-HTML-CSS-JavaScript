e1b9396d4ed144b3cbd94f3c55e844c667c687aa
csesojonmia23 <sojonmiawebdev@gmail.com>
2024-08-24 12:18:19 +0600

    LMS Responsive Website

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Nav open close
+$('#menu-btn').click(function () {
+     $('.navbar .nav-menu').addClass('active')
+});
+$('#menu-close').click(function () {
+     $('.navbar .nav-menu').removeClass('active')
+});
+
